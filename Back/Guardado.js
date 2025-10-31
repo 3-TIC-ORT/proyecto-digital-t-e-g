e4f84f78fce280f1aa00bfdda88eda0a2c535a67
Matmorow <matmorrow20@gmail.com>
2025-10-31 11:54:12 -0300

    Back Funcionando

--- Back/Guardado.js
+++ Back/Guardado.js
@@ -1,17 +1,17 @@
 import fs from "fs";
 import { subscribeGETEvent, subscribePOSTEvent, startServer } from "soquetic";
 
-function guardarFichas() {
-  JSON.stringify(fs.writeFileSync("datos.json", ));
+function guardarFichas(datos) {
+  JSON.stringify(fs.writeFileSync("datos.json", datos));
 }
 
-subscribePOSTEvent("guardar", () => guardarFichas());
+subscribePOSTEvent("guardar", (datos) => guardarFichas(datos));
 
 function cargarFichas(archivo = "fichas.json") {
-    let data = JSON.parse(fs.readFileSync("datos.json", "utf8"));
+    let data = JSON.parse(fs.readFileSync("datos.json", "utf-8"));
     return (data);
 }
 
-subscribeGETEvent("cargar", () => cargarFichas());
+subscribeGETEvent("cargar", cargarFichas);
 
 startServer();
